ab15e7c07f45ae671f725cd9e58fa0f26477161c
Dan Bergelt <dan@danbergelt.com>
2020-01-28 17:54:19 -0500

    gatsby localstorage fix take two

--- portfolio/src/context/context.js
+++ portfolio/src/context/context.js
@@ -2,9 +2,9 @@ import React, { useState, createContext } from "react"
 
 export const DarkContext = createContext()
 
-const windowGlobal = typeof window !== "undefined" && window
-
 export const DarkProvider = ({ children }) => {
+  const windowGlobal = typeof window !== "undefined" && window
+
   const [dark, setDark] = useState(
     windowGlobal.localStorage.getItem("dark") ? true : false
   )
--- portfolio/src/utils/darkmode.js
+++ portfolio/src/utils/darkmode.js
@@ -1,9 +1,9 @@
-const windowGlobal = typeof window !== "undefined" && window
-
 export const addDark = () => {
+  const windowGlobal = typeof window !== "undefined" && window
   windowGlobal.localStorage.setItem("dark", true)
 }
 
 export const removeDark = () => {
+  const windowGlobal = typeof window !== "undefined" && window
   windowGlobal.localStorage.removeItem("dark")
 }
